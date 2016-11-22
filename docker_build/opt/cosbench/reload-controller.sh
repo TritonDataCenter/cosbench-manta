26746ea14d5f016b03382f84789b34c8b800a1dd
Elijah Zupancic <elijah@zupancic.name>
2016-11-22 09:52:49 -0800

    Enabled autoreconfiguration of controller on driver scale events.

--- docker_build/opt/cosbench/reload-controller.sh
+++ docker_build/opt/cosbench/reload-controller.sh
@@ -2,8 +2,12 @@
 
 CONSUL=${CONSUL:-consul}
 
+# Templates out the list of drivers in the controllers configuration
 consul-template \
         -once \
         -dedup \
         -consul ${CONSUL}:8500 \
         -template "/opt/cosbench/conf/controller.conf.ctmpl:/opt/cosbench/conf/controller.conf"
+
+# Hot reload COSBench configuration to update the list of drivers
+echo "refresh cosbench-config" | nc -q 1 0.0.0.0 18089
