2743247e0ab3fdfd5f78949cc81bd7afc7655bdc
Tomas Celaya <tjcelaya@gmail.com>
2018-08-13 13:49:22 -0700

    Allow cosbench path and checksum to be supplied to build-docker-image.sh

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-set -ex
+set -e
+set -x
 
-COSBENCH_MANTA_PATH=$(find target -name 'cosbench-manta-*.jar' -print -quit)
-COSBENCH_MANTA_CHECKSUM=$(shasum -a256 $COSBENCH_MANTA_PATH | cut -d' ' -f1)
+if [ -z $1 ]; then
+    COSBENCH_MANTA_PATH=$(find target -name 'cosbench-manta-*.jar' -print -quit)
+    COSBENCH_MANTA_CHECKSUM=$(shasum -a256 $COSBENCH_MANTA_PATH | cut -d' ' -f1)
+else
+    COSBENCH_MANTA_PATH=$1
+    COSBENCH_MANTA_CHECKSUM=$2
+fi
 
-if [ ! -f $COSBENCH_MANTA_PATH ]; then
+if [ -z $COSBENCH_MANTA_PATH ]; then
 	>&2 echo "No cosbench-manta jar found, expecting a file named 'cosbench-manta-*.jar' in 'target' subdirectory"
+	exit 1
+fi
+
+if [ -z $COSBENCH_MANTA_CHECKSUM ]; then
+	>&2 echo "Missing SHA256 checksum"
 	exit 1
 fi
 
