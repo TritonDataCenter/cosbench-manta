ea14dec78231adbe6a86362c009a5d150519e8de
Tomas Celaya <tjcelaya@gmail.com>
2018-08-13 13:54:57 -0700

    Better error handling for ./build-docker-image.sh

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -2,22 +2,31 @@
 set -e
 set -x
 
-if [ -z $1 ]; then
-    COSBENCH_MANTA_PATH=$(find target -name 'cosbench-manta-*.jar' -print -quit)
-    COSBENCH_MANTA_CHECKSUM=$(shasum -a256 $COSBENCH_MANTA_PATH | cut -d' ' -f1)
+if [ -z $1 ] && [ -z $2 ]; then
+  >&2 echo "No arguments provided, looking for a JAR in the ./target directory"
+
+  COSBENCH_MANTA_PATH=$(find target -name 'cosbench-manta-*.jar' -print -quit)
+
+  if [ -z $COSBENCH_MANTA_PATH ]; then
+    >&2 echo 'No local build found, exiting (hint: run `mvn package`)'
+    exit 1
+  fi
+
+
+  COSBENCH_MANTA_CHECKSUM=$(shasum -a256 $COSBENCH_MANTA_PATH | cut -d' ' -f1)
 else
-    COSBENCH_MANTA_PATH=$1
-    COSBENCH_MANTA_CHECKSUM=$2
+  COSBENCH_MANTA_PATH=$1
+  COSBENCH_MANTA_CHECKSUM=$2
 fi
 
 if [ -z $COSBENCH_MANTA_PATH ]; then
-	>&2 echo "No cosbench-manta jar found, expecting a file named 'cosbench-manta-*.jar' in 'target' subdirectory"
-	exit 1
+  >&2 echo "No cosbench-manta jar supplied. Please provide a path or URL to cosbench-manta-\$VERSION.jar"
+  exit 1
 fi
 
 if [ -z $COSBENCH_MANTA_CHECKSUM ]; then
-	>&2 echo "Missing SHA256 checksum"
-	exit 1
+  >&2 echo "Missing SHA256 checksum"
+  exit 1
 fi
 
 COSBENCH_IMAGE_TAG=${COSBENCH_IMAGE_TAG:-latest}
